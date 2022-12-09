6ae2aa763c3a8fc73ee1f91fd52e15f4b5d4fbe0
Siggi <7286178+Siggi88@users.noreply.github.com>
2022-12-09 23:12:47 +0000

    Add optional offset enforcement to RafInputStream

--- src/main/java/io/siggi/transformfile/io/RafInputStream.java
+++ src/main/java/io/siggi/transformfile/io/RafInputStream.java
@@ -9,10 +9,22 @@ public class RafInputStream extends InputStream {
     private final boolean relayClose;
     private final byte[] one = new byte[1];
     private long markPos = -1L;
+    private long offset;
 
     public RafInputStream(RandomAccessFile raf, boolean relayClose) {
+        this(raf, -1L, relayClose);
+    }
+    public RafInputStream(RandomAccessFile raf, long offset, boolean relayClose) {
         this.raf = raf;
+        this.offset = offset;
         this.relayClose = relayClose;
+    }
+
+    private void seekToOffset() throws IOException {
+        if (offset < 0L) return;
+        if (raf.getFilePointer() != offset) {
+            raf.seek(offset);
+        }
     }
 
     @Override
@@ -29,15 +41,24 @@ public class RafInputStream extends InputStream {
 
     @Override
     public int read(byte[] buffer, int offset, int length) throws IOException {
-        return raf.read(buffer, offset, length);
+        seekToOffset();
+        int amount = raf.read(buffer, offset, length);
+        if (this.offset >= 0L && amount >= 0) this.offset += amount;
+        return amount;
     }
 
     @Override
     public long skip(long n) throws IOException {
-        long oldPointer = raf.getFilePointer();
+        long oldPointer = offset >= 0L ? offset : raf.getFilePointer();
         long newPointer = Math.max(0L, Math.min(raf.length(), oldPointer + n));
-        raf.seek(newPointer);
+        if (offset >= 0L) offset = newPointer;
+        else raf.seek(newPointer);
         return newPointer - oldPointer;
+    }
+
+    public void seek(long offset) throws IOException {
+        if (this.offset >= 0L) this.offset = offset;
+        else raf.seek(offset);
     }
 
     @Override
@@ -47,7 +68,7 @@ public class RafInputStream extends InputStream {
             return;
         }
         try {
-            markPos = raf.getFilePointer();
+            markPos = offset >= 0L ? offset : raf.getFilePointer();
         } catch (IOException e) {
             markPos = -1;
         }
@@ -58,7 +79,8 @@ public class RafInputStream extends InputStream {
         if (markPos == -1L) {
             throw new IOException("Mark never set");
         }
-        raf.seek(markPos);
+        if (offset >= 0L) offset = markPos;
+        else raf.seek(markPos);
     }
 
     @Override
@@ -67,7 +89,7 @@ public class RafInputStream extends InputStream {
     }
 
     public long getFilePointer() throws IOException {
-        return raf.getFilePointer();
+        return offset >= 0L ? offset : raf.getFilePointer();
     }
 
     @Override
